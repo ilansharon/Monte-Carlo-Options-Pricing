c9930d580759015e11ea8d5a020bec7452d82de7
ilansharon <isharon@umich.edu>
2025-01-11 16:46:04 -0500

    added black scholes calculation for benchmarking, added comments for readbility

--- methods.py
+++ methods.py
@@ -1,7 +1,9 @@
 from preprocessing import optionData
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
+#simlate one Geometric Brownian Motion (GBM) path
 def simulateGBM(S0, r, sigma, T, N):
     dt = T/N
 
@@ -13,6 +15,7 @@ def simulateGBM(S0, r, sigma, T, N):
         path.append(S)
     return np.array(path)
 
+#iterate GBM over Monte Carlo sample size
 def iterateGBM(S0, r, sigma, T, N, numIterations):
     allPaths = np.zeros((numIterations, N+1))  
 
@@ -21,17 +24,29 @@ def iterateGBM(S0, r, sigma, T, N, numIterations):
 
     return allPaths
 
+#get call or put payoff from final prices
 def getPayoffs(paths, K, call):
     Sfinal = paths[:, -1]
     if call:
-        return np.maximum(Sfinal-K, 0)
+        return np.maximum(Sfinal-K, 0)          #call payoff
     else:
-        return np.maximum(K-Sfinal, 0)
+        return np.maximum(K-Sfinal, 0)          #put payoff
     
+#discount payoff based on risk-free rate and calculate avg
 def avgDiscountedPayoff(payoffs, r, T):
     disc = payoffs * np.exp(-r * T)
     avgDisc = np.average(disc)
 
     return avgDisc
+
+#price with black scholes for benchmarking
+def blackScholes(S0, K, r, sigma, T , call):
+
+    d1 = (np.log(S0 / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    if call:
+        return S0 * norm.cdf(d1) - (K * np.exp(-r * T) * norm.cdf(d2))
+    else:
+        return (K * np.exp(-r * T) * norm.cdf(-d2)) - (S0 * norm.cdf(-d1))
 
 
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from preprocessing import optionData
-from methods import iterateGBM, getPayoffs, avgDiscountedPayoff
+from methods import iterateGBM, getPayoffs, avgDiscountedPayoff, blackScholes
 
 S0, K, sigma, T, r = optionData("MSFT", "2025-01-17", True)
-paths = iterateGBM(S0, r, sigma, T, 1000, 1000)
+paths = iterateGBM(S0, r, sigma, T, 10000, 10000)
 payoffs = getPayoffs(paths, K, True)
 discounted = avgDiscountedPayoff(payoffs, r, T)
+BS = blackScholes(S0, K, r, sigma, T, True)
 
 print("avg discounted payoff: ", discounted)
-
+print("black scholes payout", BS)
 # for path in paths:
 #     plt.plot(time, path)
 # plt.show()
